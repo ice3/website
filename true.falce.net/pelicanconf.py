066ad181bebe9fd5b53af5ffdca78321e43a1911
Matthieu Falce <falce.matthieu@gmail.com>
2015-09-25 18:35:17 +0200

    added pelican conf files

---
+++ true.falce.net/pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Matthieu Falce'
+SITENAME = u'true.falce.net'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+        )
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/MatthieuFalce'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME=u'mediumfox'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
