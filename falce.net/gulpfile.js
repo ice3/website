0127c4159f611abb246ec67c367452fa4b028c8d
Matthieu Falce <falce.matthieu@gmail.com>
2017-05-05 16:00:58 +0200

    style + bug fix

--- falce.net/gulpfile.js
+++ falce.net/gulpfile.js
@@ -10,17 +10,17 @@ gulp.task('default', ['bootstrap_css', 'fontawesome_css']);
 gulp.task('bootstrap_css', function() {
     gulp.src('html/assets/bootstrap-3.3.5-dist/css/bootstrap.css')
         .pipe(uncss({
-            html: ['./html/index.html']
+            html: ['./html/index.html', './html/presentation/index.html']
         }))
         .pipe(cssmin())
         .pipe(gulp.dest('html/assets/bootstrap-3.3.5-dist/uncss/'));
 });
 
 gulp.task('fontawesome_css', function() {
-    gulp.src('html/assets//font-awesome-4.4.0/css/font-awesome.css')
+    gulp.src('html/assets/font-awesome-4.4.0/css/font-awesome.css')
         .pipe(uncss({
-            html: ['./html/index.html']
+            html: ['./html/index.html', './html/presentation/index.html']
         }))
         .pipe(cssmin())
-        .pipe(gulp.dest('html/assets//font-awesome-4.4.0/uncss/'));
+        .pipe(gulp.dest('html/assets/font-awesome-4.4.0/uncss/'));
 });
